e3143eb4d981dcb272900b57c2c62de20fbe3893
il4nn <ilan.r.nissim@gmail.com>
2025-05-17 14:06:09 +0200

    added functionalities

--- app.py
+++ app.py
@@ -23,8 +23,30 @@ def load_and_initialize_table() -> Tuple[int, int, pd.DataFrame]:
     empty_row_count = row_count - df.dropna().shape[0]
     return (row_count, empty_row_count, df.dropna())
 
-row_count, empty_row_count, df = load_and_initialize_table()
-st.subheader(f'There are {empty_row_count} courses out of {row_count} without any feedback')
-st.dataframe(df,hide_index=True)
+
+with st.container():
+    row_count, empty_row_count, df = load_and_initialize_table()
+    st.subheader(f'There are {empty_row_count} courses out of {row_count} without any feedback')
+    event = st.dataframe(
+        df,
+        hide_index=True,
+        on_select="rerun",
+        use_container_width=True
+    )
+
+    selected = event.selection.rows
+    if selected:
+        course_code = df.loc[selected,'Code'].item()
+        course_name = df.loc[selected,'Name'].item()
+        st.subheader(f'Reviews for course: {course_name} - {course_code}')
+        reviews = get_reviews(course_code)
+        for review in reviews:
+            rev, semester = parse_review(review)
+            st.text(rev)
+            st.text(f"Semester: {semester}")
+            st.divider()
+
+
+
         
 
--- ethzcourses.py
+++ ethzcourses.py
@@ -2,21 +2,25 @@ import pandas as pd
 import requests
 import numpy as np
 
+session = requests.Session()
+
 def get_reviews(course_code: str) -> dict :
     url = f"https://cr.vsos.ethz.ch/getReviews?course={course_code}L"
-    response = requests.get(url,timeout=10)
     try: 
-        response_json = response.json()
+        # Reuse the TCP connections with a Session (HTTP- keep alive) instead of creating a new connection each time
+        response = session.get(url,timeout=10)
+        response.raise_for_status()
+        return response.json()
     except Exception as e:
         print(f"An error occurred: {e}")
         
 
 def get_ratings(course_code: str) -> dict:
     url = f"https://cr.vsos.ethz.ch/getRatings?course={course_code}L"
-    response = requests.get(url,timeout=10)
     try: 
-        response_json = response.json()
-        return response_json
+        response = session.get(url,timeout=1)
+        response.raise_for_status()
+        return response.json()
     except Exception as e:
         print(f"An error occurred: {e}")
 
@@ -26,3 +30,6 @@ def parse_ratings(df: pd.DataFrame, new_cols: list, course_code: str, ratings: l
     df.loc[df['Code'] == course_code, new_cols[:5]] = list(temp_df.mean())
     df.loc[df['Code'] == course_code, new_cols[5]] = len(ratings)
         
+
+def parse_review(review: dict )-> tuple[str,str]:
+    return review['Review'], review['Semester']
